0037d4506b984ba0b6af6f8a8cdb1c9c50ab3094
vensky <ivan.fedotovskikh@gmail.com>
2021-09-06 19:01:49 +0500

    Добавляет разделы Наши проекты и Интеко сейчас

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,21 @@
 'use strict';
 
+@@include('./_components/_swiper-bundle.js');
+
+const swiper = new Swiper('.projects__slider', {
+  // Optional parameters
+
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
