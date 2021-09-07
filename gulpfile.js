fbc0d69513ccc6bc4e531577550a0279ec98e56b
vensky <ivan.fedotovskikh@gmail.com>
2021-09-07 10:50:47 +0500

    Исправляет раздел Кто мы

--- gulpfile.js
+++ gulpfile.js
@@ -31,7 +31,7 @@ function watching() {
 }
 
 module.exports.start = parallel(html, css, js, serve, watching);
-module.exports.test = parallel(html, css, js);
+module.exports.test = parallel(html, css);
 module.exports.build = series(clean, html, css, js, img);
 
 module.exports.img = series(img);
