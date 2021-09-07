15103b9cd3a5c7b93ae65adcb0f4cec8a4a30ec8
vensky <ivan.fedotovskikh@gmail.com>
2021-09-07 21:46:11 +0500

    Добавляет разделы Основные принципы и Сотрудники

--- src/js/app.js
+++ src/js/app.js
@@ -13,3 +13,27 @@ const swiper = new Swiper('.projects__swiper', {
         prevEl: '.swiper-button-prev',
     },
 });
+
+const swiperPersonal = new Swiper('.personal__swiper', {
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination--personal',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next--personal',
+        prevEl: '.swiper-button-prev--personal',
+    },
+});
+
+const swiperBonus = new Swiper('.bonus__swiper', {
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination--bonus',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next--bonus',
+        prevEl: '.swiper-button-prev--bonus',
+    },
+});
